33c6144b72571ca323006ccdcd6e252cefe3499b
WuKong <15088709635@163.com>
2018-03-18 22:25:04 +0800

    修改

--- Param.java
+++ Param.java
@@ -2,18 +2,18 @@ package com.zentao;
 
 public class Param {
 
-	// SIT->App
+	// SIT->
 	public static final String MODULE_URL_801 = "http://10.200.180.45/zentao/testcase-browse-2--byModule-801.html";
-	// SIT->BOPS
+	// SIT->
 	public static final String MODULE_URL_802 = "http://10.200.180.45/zentao/testcase-browse-2--byModule-802.html";
-	// SIT->巡检端App
+	// SIT->
 	public static final String MODULE_URL_803 = "http://10.200.180.45/zentao/testcase-browse-2--byModule-803.html";
 
-	// 禅道相关URL
+	// 绂呴亾鐩稿叧URL
 	public static final String HOST = "http://10.200.180.45";
 	public static final String REFER_URL = "/zentao/company-browse.html";
 	public static final String LOGIN_URL = "http://10.200.180.45/zentao/user-login-L3plbnRhby9jb21wYW55LWJyb3dzZS5odG1s.html";
 
-	// 标记结果列表
+	// 鏍囪缁撴灉鍒楄〃
 	public static final String[] RESULT_LIST = { "n/a", "pass", "fail" };
 }
